0bcb12d26280a649e587583f05f1912bedfe86c8
Claudio Honorio da SIlva Junior <claudiohonorio963@gmail.com>
2023-11-06 12:10:32 -0300

    Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './assets/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from "./components/LoginPage";
+import "./assets/dist/output.css"
+import './assets/index.css';
 
 function App() {
   return (
--- src/main/java/org/example/CorsFilter.java
+++ src/main/java/org/example/CorsFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletResponse; // Importe HttpServletResponse
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
