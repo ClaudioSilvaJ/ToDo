82b241259b072041d565dea3c35416c956cef9b5
Claudio Honorio da SIlva Junior <claudiohonorio963@gmail.com>
2023-12-18 19:05:51 -0300

    Refractor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './assets/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from "./components/LoginPage";
+import LoginPage from "./views/commonUser/LoginPage";
 import "./assets/dist/output.css"
 import './assets/index.css';
+import Home from "./views/home";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
+            <Route path={"/"} element={<Home />} />
             <Route path="/login" element={<LoginPage />} />
         </Routes>
     </BrowserRouter>
