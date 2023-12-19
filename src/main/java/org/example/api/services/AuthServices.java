7370250736d2d6f904e4542fd4efc7dd93b11866
Gabriel Aragão Sanches <gabriel.sanches@edu.unipar.br>
2023-12-18 23:33:28 -0300

    Changes by Claudio

--- src/main/java/org/example/api/services/AuthServices.java
+++ src/main/java/org/example/api/services/AuthServices.java
@@ -1,7 +1,9 @@
 package org.example.api.services;
+import org.example.dtos.TokenDTO;
 import org.example.functions.PasswordHashing;
 import org.example.configs.MorphiaConfig;
 import org.example.dtos.LoginDTO;
+import org.example.configs.TokenGenerator;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.Query;
 
@@ -10,26 +12,36 @@ import javax.ws.rs.core.Response;
 
 @Path("/auth")
 public class AuthServices {
+
+    TokenGenerator keyGenerator = new TokenGenerator();
     Datastore datastore = new MorphiaConfig().getDatastore();
 
     @POST
     @Path("/login")
     @Consumes("application/json; charset=UTF-8")
     @Produces("application/json; charset=UTF-8")
-    public Response login(final LoginDTO dto) {
-        if (dto == null) {
+    public Response login(final LoginDTO userTryDTO) {
+        System.out.println(userTryDTO.getEmail());
+        System.out.println(userTryDTO.getPassword());
+        if (userTryDTO == null) {
             return Response.status(Response.Status.UNAUTHORIZED).entity("Please enter your login details!").build();
         }
         try {
-            Query<LoginDTO> query = datastore.createQuery(LoginDTO.class).field("email").equal(dto.getEmail());
+            Query<LoginDTO> query = datastore.createQuery(LoginDTO.class).field("email").equal(userTryDTO.getEmail());
             LoginDTO user = query.get();
             if(user == null){ return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid login").build();}
+            if(PasswordHashing.checkPassword(userTryDTO.getPassword(), user.getSalt(), user.getPassword())){
+                TokenDTO token = keyGenerator.generatorToken(userTryDTO.getEmail());
+                System.out.println(token);
+                datastore.save(token.getToken());
+                return Response.ok(token.getToken()).build();
+            }
 
-            if(PasswordHashing.checkPassword(dto.getPassword(), user.getSalt(), user.getPassword())){ return Response.ok("{\"message\":\"Login successful\"}").build(); }
-            else { return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid login").build(); }
+            else {
+                return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid login").build(); }
 
         } catch (Exception e) {
-            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid login").build();
+            return Response.status(Response.Status.NOT_IMPLEMENTED).entity("Invalid login").build();
         }
 
 
